1e2442d85d965dda3a8619b4d18186f65b0b8e7b
Alex <AlexanderDesmond848898@gmail.com>
2020-06-19 15:46:54 +1000

    Add files and ranks to definitions.

--- scripts/definitions.js
+++ scripts/definitions.js
@@ -17,3 +17,27 @@ const PIECES = {
 
 // Number of squares in the virtual board.
 const NUM_OF_SQUARES = 120;
+
+// Files and Ranks
+const FILES = {
+  FILE_A: 0,
+  FILE_B: 1,
+  FILE_C: 2,
+  FILE_D: 3,
+  FILE_E: 4,
+  FILE_F: 5,
+  FILE_G: 6,
+  FILE_H: 7,
+  FILE_NONE: 8,
+};
+const RANKS = {
+  RANK_1: 0,
+  RANK_2: 1,
+  RANK_3: 2,
+  RANK_4: 3,
+  RANK_5: 4,
+  RANK_6: 5,
+  RANK_7: 6,
+  RANK_8: 7,
+  FILE_NONE: 8,
+};
