56ff49cd5538e074d6b4d7ecaded27365f9fe854
Alex <AlexanderDesmond848898@gmail.com>
2020-06-28 10:17:18 +1000

    Add generateMove function.

---
+++ scripts/movement.ts
@@ -0,0 +1,12 @@
+// Generate move data.
+function generateMove(
+  origin: number,
+  destination: number,
+  captured: number,
+  promoted: number,
+  flag: number
+): number {
+  return (
+    origin | (destination << 7) | (captured << 14) | (promoted << 20) | flag
+  );
+}
