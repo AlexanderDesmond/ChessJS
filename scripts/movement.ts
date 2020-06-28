eb042043266d3061162a8641b05b04dd9cfb62d0
Alex <AlexanderDesmond848898@gmail.com>
2020-06-28 10:07:54 +1000

    Add flags for captured and promoted pieces.

--- scripts/movement.ts
+++ scripts/movement.ts
@@ -18,7 +18,11 @@ function getPromotedPiece(move: number): number {
   return (move >> 20) & 0xf;
 }
 
-// Add flags for En Passant, Pawn starting move, and castling
+// Flags for En Passant, Pawn starting move, and castling
 const EP_FLAG = 0x40000;
 const START_FLAG = 0x80000;
 const CASTLE_FLAG = 0x1000000;
+
+// Flags for captured and promoted pieces.
+const CAPTURED_FLAG = 0x7c000;
+const PROMOTION_FLAG = 0xf00000;
