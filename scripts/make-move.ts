ca1ff4b86a77378bde2d20232f091964f657bea2
Alex <AlexanderDesmond848898@gmail.com>
2020-06-30 11:01:56 +1000

    Add addPiece function.

--- scripts/make-move.ts
+++ scripts/make-move.ts
@@ -1,3 +1,4 @@
+// Clear piece
 function clearPiece(square: number): void {
   let piece = chessBoard.pieces[square];
   let colour = pieceColour[piece];
@@ -7,7 +8,7 @@ function clearPiece(square: number): void {
 
   // Remove the piece from the pieces array.
   chessBoard.pieces[square] = PIECES.EMPTY;
-  // Remove the piece from the material array.
+  // Remove the piece's colour from the material array.
   chessBoard.material[colour] -= pieceValue[piece];
 
   // Remove the piece from the pieceList array.
@@ -23,4 +24,23 @@ function clearPiece(square: number): void {
   chessBoard.pieceNumber[piece]--;
   chessBoard.pieceList[getPieceIndex(piece, temp)] =
     chessBoard.pieceList[getPieceIndex(piece, chessBoard.pieceNumber[piece])];
+}
+
+// Add piece
+function addPiece(piece: number, square: number): void {
+  let colour = pieceColour[piece];
+
+  // Hash the piece into the key.
+  hashPiece(piece, square);
+
+  // Add the piece to the pieces array.
+  chessBoard.pieces[square] = piece;
+  // Add the piece's colour to the material array.
+  chessBoard.material[colour] += pieceValue[piece];
+
+  // Add the piece to the pieceList array.
+  chessBoard.pieceList[
+    getPieceIndex(piece, chessBoard.pieceNumber[piece])
+  ] = square;
+  chessBoard.pieceNumber[piece]++;
 }
