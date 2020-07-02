b52d6a524d3f75de5abc52fe96a9333a209a30a7
Alex <AlexanderDesmond848898@gmail.com>
2020-07-02 13:29:20 +1000

    Add code to start performance tests.

--- scripts/perf-tests.ts
+++ scripts/perf-tests.ts
@@ -1,10 +1,10 @@
 // Performances Testing
-let leafNodes: number;
+let nodes: number;
 
 // Performance Test
 function perfTest(depth: number): void {
   if (depth === 0) {
-    leafNodes++;
+    nodes++;
     return;
   }
 
@@ -29,6 +29,42 @@ function perfTest(depth: number): void {
     perfTest(depth - 1);
     revertMove();
   }
+
+  return;
+}
+
+// Start Performance Test
+function startPerfTest(depth: number): void {
+  printBoard();
+  console.log("Starting Test to Depth: ", depth);
+  nodes = 0;
+
+  let move: number,
+    moveNum: number = 0;
+
+  // Loop through generated moves.
+  for (
+    let i = chessBoard.moveListStart[chessBoard.plyCount];
+    i < chessBoard.moveListStart[chessBoard.plyCount + 1];
+    i++
+  ) {
+    move = chessBoard.moveList[i];
+
+    // If the move was illegal, skip to next iteration of loop.
+    if (!makeMove(move)) {
+      continue;
+    }
+
+    moveNum++;
+    let nodesSum = nodes;
+    perfTest(depth - 1);
+    revertMove();
+    let oldNodes = nodes - nodesSum;
+
+    console.log("Move: " + moveNum + " " + moveToString(move) + " " + oldNodes);
+  }
+
+  console.log("Test Complete: " + nodes + " leaf nodes visited.");
 
   return;
 }
