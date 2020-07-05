3d275204f1364429eac48286945ab8ba2c9bec34
Alex <AlexanderDesmond848898@gmail.com>
2020-07-05 15:21:46 +1000

    Add storePVMove() function.

--- scripts/pv-table.ts
+++ scripts/pv-table.ts
@@ -1,10 +1,18 @@
 // Search the pvTable and return the move which matches the current board state.
 function searchPVTable(): number {
-  let index = chessBoard.boardState % PV_ENTRIES;
+  const index = chessBoard.boardState % PV_ENTRIES;
 
   if (chessBoard.pvTable[index].boardState === chessBoard.boardState) {
     return chessBoard.pvTable[index].move;
   }
 
   return NO_MOVE;
+}
+
+// Store the current board state and given move in the pvTable.
+function storePVMove(move: number): void {
+  const index = chessBoard.boardState % PV_ENTRIES;
+
+  chessBoard.pvTable[index].boardState = chessBoard.boardState;
+  chessBoard.pvTable[index].move = move;
 }
