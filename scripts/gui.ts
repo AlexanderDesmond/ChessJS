765a1ff5742b085e22b36331d0aa9af6299f8613
Alex <AlexanderDesmond848898@gmail.com>
2020-07-12 17:08:48 +1000

    Add and implement setupPieces() function.

--- scripts/gui.ts
+++ scripts/gui.ts
@@ -7,3 +7,43 @@ function submitFen(): void {
   searchPosition();
   //startPerfTest(5);
 }
+
+// Set board pieces.
+function setupPieces(): void {
+  let sq120: number, piece: number, file: number, rank: number;
+
+  // Loop through board squares.
+  for (let sq = 0; sq < 64; sq++) {
+    // Get square converted to 120 grid.
+    sq120 = to120(sq);
+    // Get piece.
+    piece = chessBoard.pieces[sq120];
+
+    // Get file and rank.
+    file = files[sq120];
+    rank = ranks[sq120];
+
+    // For every piece.
+    if (piece >= PIECES.wP && piece <= PIECES.bK) {
+      // Set up file and rank names.
+      let fileName: string = "file" + (file + 1);
+      let rankName: string = "rank" + (rank + 1);
+
+      // Set up piece file name
+      let pieceFileName: string =
+        "images/chess-pieces/" +
+        sideChar[pieceColour[piece]] +
+        pieceChar[piece].toUpperCase() +
+        ".png";
+
+      // Create new image node.
+      let node = document.createElement("img");
+      // Add piece class to new image node.
+      node.classList.add("piece", rankName, fileName);
+      // Set src attribute of new image node.
+      node.setAttribute("src", pieceFileName);
+      // Add new image node to board div.
+      document.getElementsByClassName("board")[0].appendChild(node);
+    }
+  }
+}
